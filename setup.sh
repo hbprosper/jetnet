7ea9e5c5750309889c5c71f08a52311c6837b9d5
Harrison B. Prosper <hbprosper@gmail.com>
2017-06-21 22:07:48 -0400

    add unweight.py

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export LD_LIBRARY_PATH=$DIR/lib:$LD_LIBRARY_PATH
 export JETNET_PATH=$DIR
+export PATH=$JETNET_PATH/bin:$PATH
 echo $JETNET_PATH
 
